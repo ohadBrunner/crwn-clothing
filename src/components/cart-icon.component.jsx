e76139baaee15c7e8f1e24209e69afda860cbdea
ohadBrunner <ohad27@gmail.com>
2021-12-06 15:37:05 +0200

    updated the app with Reselect, replaced cart selectors with memoized selectors

--- src/components/cart-icon.component.jsx
+++ src/components/cart-icon.component.jsx
@@ -2,21 +2,27 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingIcon } from '../assests/shopping-bag.svg';
 
 import '../sass/app.scss';
+// import CartItem from './cart-item.component';
 
 // no need to get toggleCartHidden as a prop cause it's just a function (always the same...)
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
+
+const mapStateToProps = state => ({
+  itemCount: selectCartItemsCount(state),
+});
 
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,23 @@
+import { createSelector } from 'reselect';
+import CartItem from '../../components/cart-item.component';
+
+// the reference to state is propagated from selectCartItemsCount
+const selectCart = state => state.cart;
+
+// memoized selector
+// since this selector is memoized, it would run only if it's arguments have changed
+export const selectCartItems = createSelector(
+  [selectCart],
+  cart => cart.cartItems
+);
+
+// memoized selector, use the selector selectCartItems's value to derive the relavant data
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (accumalatedQuantity, cartItem) =>
+        accumalatedQuantity + cartItem.quantity,
+      0
+    )
+);
